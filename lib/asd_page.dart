c31ae99eb08423e9474c32d75b01e3de0d9d6a3c
Artemii Oliinyk <aoliinyk@azercell.org>
2022-07-14 16:09:56 +0300

    [Artemii] Create asd_page.dart

---
+++ lib/asd_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class AsdPage extends StatelessWidget {
+  const AsdPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
